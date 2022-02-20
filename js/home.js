768cdbfec10a11c8c27c2d7716323072ef84c20a
yulgun <green4652@naver.com>
2022-02-20 14:59:17 +0900

    20220220

---
+++ js/home.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', event => {
+    // setTimeout(() => {
+    //     document.body.classList.add("reveal")
+    // }, 1000),
+    // document.addEventListener("click", () => {
+    //     document.body.classList.toggle("reveal")
+    // });
+    
+});
